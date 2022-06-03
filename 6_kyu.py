be0ab6e86fa1c2d123d3d5b5f69824628db95f56
Pravin P B <pravin.sarana@gmail.com>
2022-06-03 23:16:50 +0530

    03-06-2022

---
+++ 6_kyu.py
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
+# def array_diff(a, b):
+#     out = []
+#     for number in a:
+#         if number in b:
+#             pass
+#         else:
+#             out.append(number)
+#     return out
+
+
+# https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
+
+# def count_bits(n):
+#     binary = bin(n)
+#     return sum([int(i) for i in str(binary) if i == '1'])
+
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/python
+
+
+
+#
+
