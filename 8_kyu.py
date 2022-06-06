6698d8cd8e8956c3775d9ff1f8b84f7dc18d4c9c
Pravin P B <85824492+pravinpb@users.noreply.github.com>
2022-06-06 22:39:09 +0530

    06-06-2022 (commit)

--- 8_kyu.py
+++ 8_kyu.py
@@ -1,14 +1,14 @@
 # https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/python
 
-# def find_smallest_int(arr):
-#     return min(arr)
+def find_smallest_int(arr):
+    return min(arr)
 
 # https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
 
-# def no_space(x):
-#     return x.replace(" ",'')
+def no_space(x):
+    return x.replace(" ",'')
 
 #https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
 
-# def summation(num):
-#     return (num*(num+1))/2
+def summation(num):
+    return (num*(num+1))/2
