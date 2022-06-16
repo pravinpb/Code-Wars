58935f3cca1870f5905e6b36a6fdfa475b771818
Pravin P B <pravin.sarana@gmail.com>
2022-06-16 23:41:57 +0530

    16-06-2022

--- 8_kyu.py
+++ 8_kyu.py
@@ -12,3 +12,21 @@ def no_space(x):
 
 def summation(num):
     return (num*(num+1))/2
+
+#https://www.codewars.com/kata/54edbc7200b811e956000556/train/python
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+#https://www.codewars.com/kata/523b4ff7adca849afe000035/train/python
+def greet():
+    return 'hello world!'
+
+# https://www.codewars.com/kata/544675c6f971f7399a000e79/train/python
+def string_to_number(s):
+    return int(s)
+
+# https://www.codewars.com/kata/582cb0224e56e068d800003c/train/python
+def litres(time):
+    return round(int(time*0.5))
+
+# https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/python
